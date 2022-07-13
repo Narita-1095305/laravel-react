ca5a15fc41c84f8faf69695d71c1f19f70e5d460
narita-1095305 <b2560305@planet.kanazawa-it.ac.jp>
2022-07-14 00:12:38 +0900

    バックエンドとフロントエンドのつなぎ合わせ

--- src/resources/js/pages/Home.js
+++ src/resources/js/pages/Home.js
@@ -1,5 +1,6 @@
 
-import React　from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Button, Card } from '@material-ui/core';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import MainTable from '../components/MainTable';
@@ -15,24 +16,36 @@ const useStyles = makeStyles((theme) => createStyles({
 //ヘッダーのコンテンツ用の配列定義
 const headerList = ['名前', 'タスク内容', '編集', '完了'];
 
-let rows = [
-    {
-        name: "ウンチー",
-        content: "ウンチーコングって知ってる？",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },
-    {
-        name: "ドンキー",
-        content: "ドンキーーコングって知ってる？",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },
-];
 
 function Home() {
     //定義したスタイルを利用するための設定
     const classes = useStyles();
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        getPostsData();
+    },[]);
+    const getPostsData = () => {
+        axios
+        .get('/api/posts')
+        .then(response => {
+            setPosts(response.data);
+            console.log(response.data);
+        })
+        .catch(() => {
+            console.log('通信に失敗しました');
+        })
+    }
+    let rows = [];
+
+    posts.map((post) => 
+        rows.push({
+            name: post.name,
+            content: post.content,
+            editBtn: <Button color="secondary" variant="contained">編集</Button>,
+            deleteBtn: <Button color="primary" variant="contained">完了</Button>,
+        })
+    );
 
     return (
         <div className="container">
